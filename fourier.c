777f88f708664a18a0f7b0f5efa6741721642c55
Raiane Roza <raianeroza@id.uff.br>
2017-10-28 15:40:17 -0200

    sen3x

---
+++ fourier.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+FILE *op;
+
+double f1(double x){
+	return(sin(3*x));
+}
+
+main(int argc, char **argv)
+{
+	double *y, dx, x;
+	int i=0, cont=0;
+	
+	op=fopen(argv[1], "r");
+	
+	dx=2*M_PI/1024;
+
+	op = fopen(argv[1], "w");
+	for(x=-M_PI; x <= M_PI; x+=dx)
+		fprintf(op, "%lf\t%lf\n", x, f1(x));
+	
+	fclose(op);
+	
+	
+}
